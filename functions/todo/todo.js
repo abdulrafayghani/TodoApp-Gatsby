0064475e2817e92d7c439c7ff116f9ceec865b59
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-11-06 21:32:01 +0500

    first commit

--- functions/todo/todo.js
+++ functions/todo/todo.js
@@ -1,41 +1,74 @@
 const { ApolloServer, gql } = require('apollo-server-lambda')
+const faunadb = require('faunadb'),
+  q = faunadb.query
+require('dotenv').config()
 
 const typeDefs = gql`
   type Query {
-    hello: String
-    allAuthors: [Author!]
-    author(id: Int!): Author
-    authorByName(name: String!): Author
+      allTodos: [Todo!]
   }
-  type Author {
-    id: ID!
-    name: String!
-    married: Boolean!
+
+  type Todo {
+    todo: String
+    id: String
+  }
+
+  type Mutation {
+    addTodo(todo: String!): Todo
+    discardTodo(id: String): Todo
   }
 `
 
-const authors = [
-  { id: 1, name: 'Terry Pratchett', married: false },
-  { id: 2, name: 'Stephen King', married: true },
-  { id: 3, name: 'JK Rowling', married: false },
-]
-
 const resolvers = {
   Query: {
-    hello: () => {
-      return 'Hello, world!'
-    },
-    allAuthors: () => {
-      return authors
-    },
-    author: () => {},
-    authorByName: (root, args) => {
-      console.log('hihhihi', args.name)
-      return authors.find((x) => x.name === args.name) || 'NOTFOUND'
+    allTodo: async () => {
+      try {
+        const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET })
+
+        const result = await client.query(
+          q.Map(q.Paginate(q.Match(q.Index("all_todos"))),
+            q.Lambda(x => q.Get(x))
+          )
+        )
+
+        return result.data.map(d => {
+          return{
+            todo: d.data.todo,
+            id: d.ref.id
+          }
+        })
+      } catch (err) {
+        console.log(err)
+      }
     },
   },
-}
+  Mutation: {
+    addTodo: async (_, { todo }) => {
+      try {
+        const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET })
 
+        const result = await client.query(
+          q.Create(q.Collection("todos"), {
+            data: {
+              title: todo
+            }
+          })
+        )
+        
+        return result.data
+    } catch (err) {
+      console.log(err)
+    }
+    },
+  deleteTodo: async (_, { id }) => {  
+    const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET })
+
+    const result = await client.query(
+      q.Delete(q.Ref(q.Collection), id)        
+    )
+  }
+}
+}
 const server = new ApolloServer({
   typeDefs,
   resolvers,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,116 @@
-import React from "react"
-import { Button } from "@material-ui/core"
+import React, { useState } from "react"
+import { gql, useMutation, useQuery } from "@apollo/client"
+import { makeStyles } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const allTodos = gql`
+  {
+    allTodos {
+      todo: String
+      id: String
+    }
+  }
+`
+
+const addTodo = gql`
+  mutation addTodo($id: String) {
+    addTodo(id: $id) {
+      todo
+    }
+  }
+`
+
+const deleteTodo = gql`
+  mutation deleteTodo($id: String) {
+    discardTodo(id: $id) {
+      id
+    }
+  }
+`
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexGrow: 1,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
 
 export default function Home() {
+  const { loading, error, data, refetch } = useQuery(allTodos)
+  const [createTodo, reg] = useMutation(addTodo)
+  const [discardTodo] = useMutation(deleteTodo)
+
+  const [todo, setTodo] = useState("")
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    await createTodo({ variables: { todo } })
+    await refetch()
+  }
+
+  const handleDelete = async id => {
+    await discardTodo({ variables: { todo: id } })
+    await refetch()
+  }
+
+  function generate(element) {
+    return [0, 1, 2].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      }),
+    );
+  }
+
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
+
   return (
-    <div>
-      <Button>
-          Hello
-      </Button>
+    <div className={classes.root}>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h6" className={classes.title}>
+        </Typography>
+        <div className={classes.demo}>
+          <List dense={dense}>
+            {generate(
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <FolderIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Single-line item"
+                  secondary={secondary ? "Secondary text" : null}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            )}
+          </List>
+        </div>
+      </Grid>
     </div>
   )
 }
